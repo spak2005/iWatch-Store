d357cffb8810bb82f3a773ae9eead3cdef4a0a78
Israel Ogbonna <uzoisrael2005@gmail.com>
2024-01-10 04:42:04 -0600

    Initial Commit

---
+++ AppleWatchStore/AppleWatchStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWatchStoreApp.swift
+//  AppleWatchStore
+//
+//  Created by Israel Ogbonna on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
