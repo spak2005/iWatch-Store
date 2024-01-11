6b3929da49fa02faa37535c1c3184656b2d576cc
Israel Ogbonna <uzoisrael2005@gmail.com>
2024-01-11 06:17:30 -0600

    I just added the popular card design

---
+++ AppleWatchStore/Views/Home/GridProductItem.swift
@@ -0,0 +1,63 @@
+//
+//  GridProductItem.swift
+//  AppleWatchStore
+//
+//  Created by Israel Ogbonna on 1/11/24.
+//
+
+import SwiftUI
+
+struct GridProductItem: View {
+    var body: some View {
+        ZStack(alignment: .bottom){
+            VStack {
+                ZStack{
+                    Image(.sportBandProductRedL)
+                        .resizable()
+                        .frame(width:250, height: 250)
+                    
+                    Image(.aluminumMidnightL)
+                        .resizable()
+                        .frame(width:250, height: 250)
+                }
+                VStack {
+                    Text("$").ultraLight() + Text("999").heavy()
+                    Text("Starlight Aluminium Case")
+                        .condensed(.bold, size: 14)
+                    Text("Braided Solo Loop")
+                        .condensed(.light, size: 14)
+                }
+                .foregroundStyle(.primary)
+                .padding(.bottom, 5)
+                
+                HStack{
+                    ForEach(0 ..< 3){ item in
+                        Circle()
+                            .frame(width:20, height:20)
+                    }
+                    
+                }
+                .padding(.bottom)
+            }
+        }
+        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Constants.gradient)
+        )
+        .overlay(alignment: .topTrailing){
+            heart
+        }
+    }
+}
+var heart: some View{
+    Image(systemName: "heart")
+        .symbolVariant(.none)
+        .font(.system(size:24))
+        .padding(.trailing, 20)
+        .padding(.top, 10)
+}
+
+#Preview {
+    GridProductItem()
+}
